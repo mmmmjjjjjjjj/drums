edfe3ab760edd41ecd4ca7adfaf1790e98a8aa69
mmmmjjjjjjjj <64771406+mmmmjjjjjjjj@users.noreply.github.com>
2020-09-01 16:36:35 -0400

    many edits
    many edits

--- sketch.js
+++ sketch.js
@@ -1,9 +1,38 @@
+let audio
+
+function preload(){
+    soundFormats('mp3');
+    audio = loadSound("assets/SITE.mp3");
+    audio.playMode('sustain');
+}
 function setup() {
-    createCanvas(canvasWidth, canvasHeight, WEBGL);
-    background(55);
+    createCanvas(displayWidth, displayHeight, WEBGL);
+    background(random(1,255));
+    
 }
 function draw() {
-    CENTER;
-    fill(map(mouseX, 0, 127))
-    ellipse(width/2, height/2, 100, 100)
-}
+    CENTER
+    let volume = map(mouseX, 0, width, 0, 1);
+    
+    volume = constrain(volume, 0, 1);
+    audio.amp(volume);
+
+  // Set the rate to a range between 0.1 and 4
+  // Changing the rate alters the pitch
+    let speed = map(mouseY, 0.1, height, 0, 2);
+    speed = constrain(speed, 0.01, 4);
+    audio.rate(speed);
+    
+    if (mouseIsPressed) {    
+        stroke(random(1,255), random(1, 255), random(1, 255));
+        fill(random(1,255), random(1, 255), random(1, 255));
+        ellipse(mouseX, mouseY, random(1,100), random(1, 100));
+        audio.play()
+      } else {
+        stroke(255);
+        fill(0);
+        audio.stop()
+      }
+    
+    }
+    
