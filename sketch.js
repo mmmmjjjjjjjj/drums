2dde9aaca2a14316b003ff2466c6a838551a0f34
mmmmjjjjjjjj <64771406+mmmmjjjjjjjj@users.noreply.github.com>
2021-04-08 22:10:01 -0700

    audio update
    update sound file, bio

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ let audio
 
 function preload(){
     soundFormats('mp3');
-    audio = loadSound("assets/SITE.mp3");
+    audio = loadSound("assets/432.mp3");
     audio.playMode('sustain');
   }
 function resetSketch() {
