418d81128cb4ba63fffa800931155285af950bc1
mmmmjjjjjjjj <64771406+mmmmjjjjjjjj@users.noreply.github.com>
2024-01-21 23:04:02 -0800

    massive update
    finally incorporated Elements

--- sketch.js
+++ sketch.js
@@ -1,63 +1,127 @@
-let audio
+let gongSound;
+let filter, reverb;
 
-function preload(){
-    soundFormats('mp3');
-    audio = loadSound("assets/432.mp3");
-    audio.playMode('sustain');
-  }
-function resetSketch() {
-    background(random(50,180), random(50, 180), 255);
-    stroke(255);
-    fill(0);
-    audio.stop();
-  }
-function setup() {
-  
-    createCanvas(window.innerWidth, window.innerHeight);
-    resetSketch(); 
-    background(random(50,180), random(50, 180), 255);
-    let col = color(255, 255, 255);
-    let button = createButton("RESET SKETCH");
-    button.style('background-color', col);
-    button.style("font-family","Helvetica");
-    button.position(window.innerWidth - 235, 5);
-    button.mousePressed(resetSketch)
-    
+let elements = []; // Array to store Element objects
+const maxSpeed = 3;
+const minElementSize = 10;
+const maxElementSize = 50;
+
+function preload() {
+  // Load the gong sound effect
+  gongSound = loadSound("assets/GONG.mp3");
 }
 
-function draw() {
-    let volume = map(mouseX, 0, window.innerWidth, 0, 1);
-    
-    volume = constrain(volume, 0, 1);
-    audio.amp(volume);
+function resetSketch() {
+  background(random(50, 180), random(50, 180), 255);
+  stroke(255);
+  fill(0);
+  elements = []; // Reset the array of elements
+}
 
-  // Set the rate to a range between 0.1 and 4
-  // Changing the rate alters the pitch
-    let speed = map(mouseY, 0.1, window.innerHeight, 0, 2);
-    speed = constrain(speed, 0.01, 4);
-    audio.rate(speed);
-    
-    if (mouseIsPressed) {    
-        stroke(random(1,255), random(1, 255), random(1, 255));
-        fill(255, random(1, 255), random(1, 255));
-        ellipse(mouseX, mouseY, random(1,100), random(1, 100));
-        audio.play();
-      } else {
-        stroke(255);
-        fill(0);
-        audio.stop();
-      }
-    }
-    
-function windowResized(){
-  resizeCanvas(windowWidth, windowHeight);
-  createCanvas(windowWidth, windowHeight);
-  resetSketch(); 
-  background(random(50,180), random(50, 180), 255);
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+  resetSketch();
+
+  // Setup the filter
+  filter = new p5.LowPass();
+  gongSound.disconnect(); // Disconnect from the master output
+  gongSound.connect(filter);
+
+  // Setup the reverb
+  reverb = new p5.Reverb();
+  gongSound.connect(reverb);
+  reverb.amp(1); // Adjust reverb amplitude
+
   let col = color(255, 255, 255);
   let button = createButton("RESET SKETCH");
   button.style('background-color', col);
-  button.style("font-family","Helvetica");
+  button.style("font-family", "Helvetica");
   button.position(window.innerWidth - 235, 5);
-  button.mousePressed(resetSketch)
+  button.mousePressed(resetSketch);
+}
+
+function draw() {
+  // Update and draw elements
+  for (let i = elements.length - 1; i >= 0; i--) {
+    elements[i].updateElement();
+    elements[i].drawElement();
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  resetSketch();
+  background(random(50, 180), random(50, 180), 255);
+
+  let col = color(255, 255, 255);
+  let button = createButton("RESET SKETCH");
+  button.style('background-color', col);
+  button.style("font-family", "Helvetica");
+  button.position(window.innerWidth - 235, 5);
+  button.mousePressed(resetSketch);
+}
+
+// Element class
+class Element {
+  constructor(x, y) {
+    this.posX = x;
+    this.posY = y;
+    this.dirX = random(-1, 1) * maxSpeed;
+    this.dirY = random(-1, 1) * maxSpeed;
+    this.size = random(minElementSize, maxElementSize);
+    this.R = random(255);
+    this.B = random(255);
+  }
+
+  updateElement() {
+    this.posX += this.dirX;
+    this.posY += this.dirY;
+    this.checkEdges();
+  }
+
+  drawElement() {
+    noFill();
+    stroke(this.R, 0, this.B, 35);
+    ellipse(this.posX, this.posY, this.size, this.size);
+    noStroke();
+    fill(255, 35);
+    ellipse(this.posX, this.posY, 2, 2);
+  }
+
+  checkEdges() {
+    if (this.posX < 0 || this.posX > width) {
+      this.dirX *= -1;
+      this.playGongSound();
+    }
+    if (this.posY < 0 || this.posY > height) {
+      this.dirY *= -1;
+      this.playGongSound();
+    }
+  }
+
+  playGongSound() {
+    // Adjust pitch based on the size of the drawn element
+    let pitch = map(this.size, minElementSize, maxElementSize, 1.5, 0.5);
+    pitch = constrain(pitch, 0.5, 1.5);
+    
+    // Set the pitch and trigger the gong sound effect
+    gongSound.rate(pitch);
+    gongSound.amp(0.5);
+    gongSound.play();
+
+    // Adjust filter and reverb parameters based on the position of the element
+    let filterFreq = map(this.posX, 0, width, 20, 2000);
+    let filterRes = map(this.posY, 0, height, 0.1, 5);
+    filter.freq(filterFreq);
+    filter.res(filterRes);
+
+    let reverbTime = map(this.posX, 0, width, 1, 10);
+    let reverbDecay = map(this.posY, 0, height, 0.1, 1);
+    reverb.process(gongSound, reverbTime, reverbDecay);
+  }
+}
+
+function mousePressed() {
+  // Create a new element at the mouse position when mouse is pressed
+  elements.push(new Element(mouseX, mouseY));
 }
