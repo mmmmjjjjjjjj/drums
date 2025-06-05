0284905c578b7d4c2768f6319a1c796fd516e9fd
mmmmjjjjjjjj <64771406+mmmmjjjjjjjj@users.noreply.github.com>
2025-06-04 21:16:00 -0700

    package-lock update
    new package

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ export default defineConfig({
   appType: 'spa',
   build: {
     // Add this line:
+    
     outDir: 'docs' // <--- Tell Vite to build into the 'docs' folder
   }
   // Make sure commas are correct if other options exist
